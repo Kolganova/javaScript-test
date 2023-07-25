07893a5ac0545b08d4c7d2f16bec385a8012c8a9
Diana Kolganova <glebova_diana@bk.ru>
2023-07-25 17:32:24 +0300

    Initial commit
    *
    git remote add origin https://github.com/Kolganova/JavaScript-test.git

---
+++ js-test/js/javaScriptTestFile.js
@@ -0,0 +1,12 @@
+﻿console.log("JS говорит привет!");
+if (confirm("Are you sure?")) {
+    if (prompt("Plese, sign here")) {
+        alert("You signed documnets")
+    }
+    else {
+        alert("You did not sign documnets");   }
+    
+}
+else {
+    alert("That's fine...");
+}
